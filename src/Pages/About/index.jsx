09a66238a2b6a283f1c2a0f0eaeed51f67e84ae1
FidanHuseynzade <huseynzadefidan7@gmail.com>
2024-05-27 00:14:43 +0400

    fixed

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -13,7 +13,7 @@ const About = () => {
       </Helmet>
       <img src="https://www.leti-cea.com/cea-tech/leti/PublishingImages/Recherche%20appliqu%C3%A9e/Plateformes/telecom/banner.jpg" alt="" />
       <div className="aboutt">
-        <p>"Telecom Invest" LLC is one of the leading operators successfully operating in the field of telecommunications and information technologies in Azerbaijan since 2007. Currently, "Telekom Invest" has two large business networks: Wireless communication services and Internet service services and Radio telecommunications</p>
+        <p>"Telecom Invest" LLC is one of the leading operators successfully operating in the field of telecommunications and information technologies in Azerbaijan since 2007. Currently, "Telecom Invest" has two large business networks: Wireless communication services and Internet service services and Radio telecommunications</p>
         <p>Telecom Invest is the largest wireless communication and IT company in Azerbaijan. He has implemented many projects that have been successfully completed in 11 years. The main goal of the company is to create a perfect system and control all projects, from management software to operations and the entire system. At the same time, Telekom Invest ISP has created a payment system for business.</p>
       </div>
       <Map />
